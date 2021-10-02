f580da164cf18ad9ec065aee7dada6957e1a3692
Phùng Xuân Đoan <kanpitest@gmail.com>
2021-10-02 14:29:58 +0700

    zxc

---
+++ 1911065522_LAB2/1911065522_LAB2/BAI1.Designer.cs
@@ -0,0 +1,188 @@
+﻿
+namespace _1911065522_LAB2
+{
+    partial class BAI1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.lbNumber1 = new System.Windows.Forms.Label();
+            this.lbNumber2 = new System.Windows.Forms.Label();
+            this.lblAnwser = new System.Windows.Forms.Label();
+            this.txtNumber1 = new System.Windows.Forms.TextBox();
+            this.txtNumber2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnMul = new System.Windows.Forms.Button();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.btnSub = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(206, 207);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(25, 25);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(206, 149);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.Size = new System.Drawing.Size(156, 20);
+            this.txtAnswer.TabIndex = 1;
+            // 
+            // lbNumber1
+            // 
+            this.lbNumber1.AutoSize = true;
+            this.lbNumber1.Location = new System.Drawing.Point(115, 82);
+            this.lbNumber1.Name = "lbNumber1";
+            this.lbNumber1.Size = new System.Drawing.Size(50, 13);
+            this.lbNumber1.TabIndex = 2;
+            this.lbNumber1.Text = "Number1";
+            // 
+            // lbNumber2
+            // 
+            this.lbNumber2.AutoSize = true;
+            this.lbNumber2.Location = new System.Drawing.Point(115, 118);
+            this.lbNumber2.Name = "lbNumber2";
+            this.lbNumber2.Size = new System.Drawing.Size(50, 13);
+            this.lbNumber2.TabIndex = 2;
+            this.lbNumber2.Text = "Number2";
+            // 
+            // lblAnwser
+            // 
+            this.lblAnwser.AutoSize = true;
+            this.lblAnwser.Location = new System.Drawing.Point(115, 156);
+            this.lblAnwser.Name = "lblAnwser";
+            this.lblAnwser.Size = new System.Drawing.Size(42, 13);
+            this.lblAnwser.TabIndex = 2;
+            this.lblAnwser.Text = "Answer";
+            // 
+            // txtNumber1
+            // 
+            this.txtNumber1.Location = new System.Drawing.Point(206, 75);
+            this.txtNumber1.Name = "txtNumber1";
+            this.txtNumber1.Size = new System.Drawing.Size(156, 20);
+            this.txtNumber1.TabIndex = 1;
+            // 
+            // txtNumber2
+            // 
+            this.txtNumber2.Location = new System.Drawing.Point(206, 111);
+            this.txtNumber2.Name = "txtNumber2";
+            this.txtNumber2.Size = new System.Drawing.Size(156, 20);
+            this.txtNumber2.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(0, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            // 
+            // btnMul
+            // 
+            this.btnMul.Location = new System.Drawing.Point(268, 207);
+            this.btnMul.Name = "btnMul";
+            this.btnMul.Size = new System.Drawing.Size(25, 25);
+            this.btnMul.TabIndex = 0;
+            this.btnMul.Text = "*";
+            this.btnMul.UseVisualStyleBackColor = true;
+            this.btnMul.Click += new System.EventHandler(this.btnMul_Click);
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.Location = new System.Drawing.Point(299, 207);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(25, 25);
+            this.btnDiv.TabIndex = 0;
+            this.btnDiv.Text = "/";
+            this.btnDiv.UseVisualStyleBackColor = true;
+            this.btnDiv.Click += new System.EventHandler(this.btnDiv_Click);
+            // 
+            // btnSub
+            // 
+            this.btnSub.Location = new System.Drawing.Point(237, 207);
+            this.btnSub.Name = "btnSub";
+            this.btnSub.Size = new System.Drawing.Size(25, 25);
+            this.btnSub.TabIndex = 0;
+            this.btnSub.Text = "-";
+            this.btnSub.UseVisualStyleBackColor = true;
+            this.btnSub.Click += new System.EventHandler(this.btnSub_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(0, 0);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 0;
+            // 
+            // BAI1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lblAnwser);
+            this.Controls.Add(this.lbNumber2);
+            this.Controls.Add(this.lbNumber1);
+            this.Controls.Add(this.txtNumber1);
+            this.Controls.Add(this.txtNumber2);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.btnDiv);
+            this.Controls.Add(this.btnMul);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnSub);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "BAI1";
+            this.Text = "BAI1";
+            this.Load += new System.EventHandler(this.BAI1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.Label lbNumber1;
+        private System.Windows.Forms.Label lbNumber2;
+        private System.Windows.Forms.Label lblAnwser;
+        private System.Windows.Forms.TextBox txtNumber1;
+        private System.Windows.Forms.TextBox txtNumber2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnMul;
+        private System.Windows.Forms.Button btnDiv;
+        private System.Windows.Forms.Button btnSub;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
---
+++ 1911065522_LAB2/1911065522_LAB2/BAI1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1911065522_LAB2
+{
+    public partial class BAI1 : Form
+    {
+        public BAI1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try {
+                float number1 = float.Parse(txtNumber1.Text);
+                float number2 = float.Parse(txtNumber2.Text);
+                float result = number1 + number2;
+                txtAnswer.Text = result.ToString();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void btnSub_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                float number1 = float.Parse(txtNumber1.Text);
+                float number2 = float.Parse(txtNumber2.Text);
+                float result = number1 - number2;
+                txtAnswer.Text = result.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnMul_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                float number1 = float.Parse(txtNumber1.Text);
+                float number2 = float.Parse(txtNumber2.Text);
+                float result = number1 * number2;
+                txtAnswer.Text = result.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void BAI1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                float number1 = float.Parse(txtNumber1.Text);
+                float number2 = float.Parse(txtNumber2.Text);
+                float result = number1 / number2;
+                txtAnswer.Text = result.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
